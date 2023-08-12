cbf1ba589e8050f54b4815715895916dae18c301
RAJKAMAL PANTHAGANI <115925450+rajkamalprls@users.noreply.github.com>
2023-08-12 15:26:57 +0530

    Update todolist.py

--- CODSOFT-TASK1-TODO-LIST/todolist.py
+++ CODSOFT-TASK1-TODO-LIST/todolist.py
@@ -2,75 +2,70 @@ from tkinter import *
 from tkinter import ttk
 
 
-class todo:
+class TodoApp:
     def __init__(self, root):
         self.root = root
-        self.root.title('To-Do-List')
-        self.root.geometry('650x410+300+150')
+        self.root.title('To-Do List')
+        self.root.geometry('680x650')
 
-
-        self.label = Label(self.root, text='To-Do-List-App',
-                font='ariel, 25 bold', width=10,bd=5, bg='green', fg='black')
+        self.label = Label(self.root, text='To-Do List App', font='Arial 25 bold', width=10, bd=5, bg='green', fg='black')
         self.label.pack(side='top', fill=BOTH)
 
-
-        self.label2 = Label(self.root, text='Add Task',
-                font='ariel, 18 bold', width=10,bd=5, bg='yellow', fg='black')
+        self.label2 = Label(self.root, text='Add Task', font='Arial 18 bold', width=10, bd=5, bg='yellow', fg='black')
         self.label2.place(x=40, y=54)
 
-        
-        self.label3 = Label(self.root, text='Tasks',
-                font='ariel, 18 bold', width=10,bd=5, bg='orange', fg='black')
+        self.label3 = Label(self.root, text='Tasks', font='Arial 18 bold', width=10, bd=5, bg='orange', fg='black')
         self.label3.place(x=320, y=54)
 
-
-        self.main_text = Listbox(self.root, height=9, bd=5, width=23, font="ariel, 20 italic bold")
+        self.main_text = Listbox(self.root, height=9, bd=5, width=23, font="Arial 20 italic bold")
         self.main_text.place(x=280, y=100)
 
-        self.text = Text(self.root, bd=5, height=2, width=30, font='ariel, 10 bold')
+        self.text = Text(self.root, bd=5, height=2, width=30, font='Arial 10 bold')
         self.text.place(x=20, y=120)
 
+        self.selected_index = None
 
         def add():
             content = self.text.get(1.0, END)
             self.main_text.insert(END, content)
             with open('data.txt', 'a') as file:
                 file.write(content)
-                file.seek(0)
-                file.close()
             self.text.delete(1.0, END)
 
         def delete():
             delete_ = self.main_text.curselection()
-            look = self.main_text.get(delete_)
-            with open('data.txt', 'r+') as f:
-                new_f = f.readlines()
-                f.seek(0)
-                for line in new_f:
-                    item = str(look)
-                    if item not in line:
-                        f.write(line)
-                f.truncate()
-            self.main_text.delete(delete_)
+            if delete_:
+                self.main_text.delete(delete_)
 
+        def clear_all():
+            self.main_text.delete(0, END)
+            with open('data.txt', 'w') as file:
+                file.truncate()
 
-            with open('data.txt', 'r') as file:
-                read = file.readlines()
-                for i in read:
-                    ready = i.split()
-                    self.main_text.insert(END,ready)
-                file.close()
+        def update():
+            selected_index = self.main_text.curselection()
+            if selected_index:
+                self.selected_index = selected_index
+                content = self.main_text.get(selected_index)
+                self.text.delete(1.0, END)
+                self.text.insert(END, content)
 
-        self.button = Button(self.root, text="Add", font='sarif, 20 bold italic', width=10,bd=5, bg='green', fg='black', command=add)
-        self.button.place(x=30, y=180)
+        self.button_add = Button(self.root, text="Add", font='Arial 20 bold italic', width=10, bd=5, bg='green', fg='black', command=add)
+        self.button_add.place(x=30, y=180)
 
-        self.button2 = Button(self.root, text="Delete", font='sarif, 20 bold italic', width=10,bd=5, bg='red', fg='black', command=delete)
-        self.button2.place(x=30, y=260)
+        self.button_delete = Button(self.root, text="Delete", font='Arial 20 bold italic', width=10, bd=5, bg='red', fg='black', command=delete)
+        self.button_delete.place(x=30, y=260)
+
+        self.button_clearall = Button(self.root, text="Clearall", font='Arial 20 bold italic', width=10, bd=5, bg='red', fg='black', command=clear_all)
+        self.button_clearall.place(x=30, y=330)
+
+        self.button_update = Button(self.root, text="Update", font='Arial 20 bold italic', width=10, bd=5, bg='blue', fg='black', command=update)
+        self.button_update.place(x=30, y=400)
 
 
 def main():
     root = Tk()
-    ui = todo(root)
+    ui = TodoApp(root)
     root.mainloop()
 
 if __name__ == "__main__":
